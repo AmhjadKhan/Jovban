2ffa6c99447d62cc55f3a50a3465deee718da731
Amhjadkhan <amjadpogramer@gmail.com>
2023-12-15 01:40:57 +0400

    jobs json collection changes

--- jobvan/src/Components/jobssec/JobCard.jsx
+++ jobvan/src/Components/jobssec/JobCard.jsx
@@ -12,12 +12,12 @@ const JobCard = ({ service }) => {
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{service.jobtitle}</h2>
+          <h2 className="card-title text-3xl">{service.jobtitle}</h2>
          <div className="flex justify-between">
-         <p>{service.Deadline}</p>
-         <p>{service.minimum_price}</p>
+         <p className="text-xl">Date:{service.Deadline}</p>
+         <p className="text-xl">price:{service.minimum_price}</p>
          </div>
-         <p>Moporem ipsum dolor sit amet consectetur adipisicing elit. Enim deleniti dicta illo, quod maiores hic sit delectus fugiat animi perspiciatis?</p>
+         <p className="text-center">Moporem ipsum dolor sit amet consectetur adipisicing elit. Enim deleniti dicta illo, quod maiores hic sit delectus fugiat animi perspiciatis?</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">BID NOW</button>
           </div>
